c6a47dcbc5f352b5e740b6a38a6cdc49fbfa3aef
EmilyLynch2023 <74692123+EmilyLynch2023@users.noreply.github.com>
2022-04-19 10:00:31 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.calendarapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.calendarfx.view;
+
+    opens com.example.calendarapp to javafx.fxml;
+    exports com.example.calendarapp;
+}
